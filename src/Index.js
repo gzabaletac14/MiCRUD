b5f973f120e2d0e596402618d48878a9fe5e56eb
German Zabaleta <gzabaletac14@gmail.com>
2021-02-12 15:09:52 -0500

    Agregar archivos

--- src/Index.js
+++ src/Index.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('handlebars');
+const methodoverride = require('method-override');
+const session = require('express-session');
 
 //initializations
 const app = express();
 //setting
-app.set('port', process.env.PORT || 3000); //configuramos el puerto del server
+//configuramos el puerto del server
+app.set('port', process.env.PORT || 3000); 
+//sacar la ruta con path de la carpeta views
 app.set('views', path.join(__dirname,'views'));
+
+//para las vistas de la aplicacion
+app.engine('.hbs',exphbs({
+defaultLayout: 'main',
+layoutsDir: path.join(app.get('views'),'layouts'),
+partialsDir:path.join(app.get('views'),'partials'),
+extname: '.hbs'
+}));
+app.set('view engine','.hbs');
+
 //middlewares
+
+//para que un formulario al enviar un dato se pueda entender 
+app.use(express.urlencoded({extended:false}));
+//para enviar otros tipos de metodos en la peticion
+app.use(methodoverride('_method'));
+
+//para guardar la informacion de session del usuario logeado
+app.use(session({
+   secret: 'mysecretapp',
+   resave: true,
+   saveUninitialized: true
+}));
+
+
 //global variables
 //routes
+app.use(require('./routes/index'));
+app.use(require('./routes/users'));
+app.use(require('./routes/notes'));
+
 //static files
 //start server
 app.listen(app.get('port'), () =>{
